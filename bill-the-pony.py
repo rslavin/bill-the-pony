17c7bd54f950b42332979fb5b7f448e69899fdfb
rslavin <rocky.slavin@gmail.com>
2023-08-14 19:10:32 -0500

    changed to face detection instead of body detection

--- bill-the-pony.py
+++ bill-the-pony.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
-import cv2
 import RPi.GPIO as GPIO
+import cv2
 import sys
 
 # GPIO pin numbers
@@ -10,11 +10,15 @@ LED_CENTER_PIN = 20
 LED_RIGHT_PIN = 21
 # width and height to resize image before sending to model
 # smaller => faster, larger => more accurate
-IMG_MODEL_SZ_PX = 150
+FRAME_MODEL_WIDTH = 320
+FRAME_MODEL_HEIGHT = 200
 # width and height to resize for display
-IMG_DISPLAY_SZ_PX = 720
+FRAME_DISPLAY_WIDTH = 540
+FRAME_DISPLAY_HEIGHT = 300
 # size in pixels of center region of image (IMG_MODEL_SIZE_PX)
 CENTER_SIZE_PX = 15
+# path to cascade classifier facial layout data
+CASCADE_PATH = '/usr/share/opencv4/lbpcascades/lbpcascade_frontalface_improved.xml'
 
 
 def init_rpi_pins():
@@ -36,6 +40,7 @@ def set_lights(light_states):
     :param light_states: List of integers: [left, center, right] with 0 for off and 1 for on.
     :return:
     """
+
     GPIO.output(LED_LEFT_PIN, GPIO.HIGH if light_states[0] else GPIO.LOW)
     GPIO.output(LED_CENTER_PIN, GPIO.HIGH if light_states[1] else GPIO.LOW)
     GPIO.output(LED_RIGHT_PIN, GPIO.HIGH if light_states[2] else GPIO.LOW)
@@ -51,22 +56,26 @@ def watch(show_video=True):
     # initialize rpi
     init_rpi_pins()
 
-    # initialize the HOG descriptor/person detector
-    hog = cv2.HOGDescriptor()
-    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    # initialize the cascade classifierA
+    classifier = cv2.CascadeClassifier(CASCADE_PATH)
+
     if show_video:
         cv2.startWindowThread()
-    # open webcam video stream
+    # open webcam video stream and set dimensions
     stream = cv2.VideoCapture(0)
+    stream.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_MODEL_WIDTH)
+    stream.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_MODEL_HEIGHT)
 
     while True:
 
-        # capture frame and resize based on speed/accuracy preferences
-        frame = cv2.resize(stream.read()[1], (IMG_MODEL_SZ_PX, IMG_MODEL_SZ_PX))
+        # capture frame and convert to grayscale for improved accuracy
+        frame = stream.read()[1]
+        # frame = cv2.flip(frame, -1)
+        gray_frame = cv2.equalizeHist(cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY))
 
-        # detect people in the image
+        # detect faces in the frame
         # returns the bounding boxes for the detected objects
-        boxes = hog.detectMultiScale(frame, winStride=(1, 1), scale=1.05)[0]
+        boxes = classifier.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=3, flags=0, minSize=(5, 5))
         boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
         centers = []
 
@@ -75,7 +84,7 @@ def watch(show_video=True):
             # x coordinate of center of box
             x_pos = ((box[2] - box[0]) / 2) + box[0]
             # x distance from center
-            x_pos_rel_center = x_pos - IMG_MODEL_SZ_PX / 2
+            x_pos_rel_center = x_pos - FRAME_MODEL_WIDTH / 2
             centers.append(
                 {'box': box, 'x_pos_rel_center': x_pos_rel_center, 'dist_to_center_x': abs(x_pos_rel_center)})
 
@@ -100,14 +109,15 @@ def watch(show_video=True):
             elif center_box_pos_x <= -CENTER_SIZE_PX:
                 print("LEFT--------------")
                 set_lights([1, 0, 0])
-            print(f"Position: {center_box_pos_x} / {IMG_MODEL_SZ_PX} px")
+            print(f"Position: {center_box_pos_x} / {FRAME_MODEL_WIDTH} px")
         else:
             set_lights([0, 0, 0])
             print("------------------")
 
         # display the resulting frame
         if show_video:
-            frame = cv2.resize(frame, (IMG_DISPLAY_SZ_PX, IMG_DISPLAY_SZ_PX))
+            frame = cv2.resize(frame, (FRAME_DISPLAY_WIDTH, FRAME_DISPLAY_HEIGHT))
+            # frame = cv2.flip(frame, 1)
             cv2.imshow("Bill the Pony", frame)
 
         # TODO fix this
@@ -119,6 +129,6 @@ def watch(show_video=True):
 
 
 if __name__ == "__main__":
-    # TODO args for everything
+    # TODO args for everything (flip, model location, etc.)
     # if any args are passed, don't show video (i.e., command line mode)
     watch(len(sys.argv) == 1)
